ef5812591855301d17af1c4b9f3fcc71743d4915
Juan Morales <juanguillermo93@gmail.com>
2023-02-22 18:30:52 -0500

    creación metodos para adicionar y eliminar cuerpos celestes

--- ejercicio1/SistemaPlanetario.java
+++ ejercicio1/SistemaPlanetario.java
@@ -10,4 +10,11 @@ public class SistemaPlanetario {
     private static final double G = 6.6743e-11; // Constante de gravitación universal
     private List<CuerpoCeleste> cuerpos;
     
+    public void agregarCuerpo(CuerpoCeleste cuerpo) {
+        cuerpos.add(cuerpo);
+    }
+
+    public void eliminarCuerpo(CuerpoCeleste cuerpo) {
+        cuerpos.remove(cuerpo);
+    }
 }
