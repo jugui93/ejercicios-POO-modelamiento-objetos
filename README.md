142b796195b5df3e69254e0ec6c43094388e5dbd
Juan Morales <juanguillermo93@gmail.com>
2023-02-22 18:28:32 -0500

    Creacion clases SistemaPlanetario y CuerpoCeleste

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ejercicios-POO-modelamiento-objetos
+Desarrollo de ejercicios de programación orientada a objetos y modelamiento de objetos
---
+++ ejercicio1/SistemaPlanetario.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+import java.util.List;
+
+/**
+ * SistemaPlanetario
+ */
+public class SistemaPlanetario {
+
+    private static final double G = 6.6743e-11; // Constante de gravitación universal
+    private List<CuerpoCeleste> cuerpos;
+    
+}
