44cb882c1d83464beb2626d74a7eef2d99bb2079
Juan Morales <juanguillermo93@gmail.com>
2023-02-22 18:39:01 -0500

    definir propiedades de la clase cuerpo celeste y metodo constructor

--- ejercicio1/CuerpoCeleste.java
+++ ejercicio1/CuerpoCeleste.java
@@ -1,5 +1,21 @@
 package ejercicio1;
 
 public class CuerpoCeleste {
+    private double masa; //masa en kilogramos
+    private double densidad;//kilogramos/m^3
+    private double diametro;//metros
+    private double distanciaAlSol;//metros
+    private int id;
+    private String nombre;
+    
+    public CuerpoCeleste(double masa, double densidad, double diametro, double distanciaAlSol, int id, String nombre) {
+        this.masa = masa;
+        this.densidad = densidad;
+        this.diametro = diametro;
+        this.distanciaAlSol = distanciaAlSol;
+        this.id = id;
+        this.nombre = nombre;
+    }
+
     
 }
