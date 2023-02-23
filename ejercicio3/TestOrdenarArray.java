b8c3a6c19e1cca3a5c108515651c583d2d2c3606
Juan Morales <juanguillermo93@gmail.com>
2023-02-22 21:23:10 -0500

    definicion metodo burbuja de ordenamiento

--- ejercicio3/TestOrdenarArray.java
+++ ejercicio3/TestOrdenarArray.java
@@ -8,5 +8,17 @@ public class TestOrdenarArray {
         System.out.print("Ingrese la longitud del array: ");
         int n = scanner.nextInt();
         OrdenarArray ordenarArreglo = new OrdenarArray(n);
+        System.out.print("Ingrese el tipo de ordenamiento (1: Burbuja, 2: QuickSort): ");
+        int tipo = scanner.nextInt();
+
+        if (tipo == 1) {
+            ordenarArreglo.burbuja(ordenarArreglo.getArreglo());
+        } else if (tipo == 2) {
+            ordenarArreglo.quickSort(ordenarArreglo.getArreglo(), 0, n - 1);
+        } else {
+            System.out.println("Tipo de ordenamiento inválido.");
+            return;
+        }
+        System.out.println("Array ordenado: " + Arrays.toString(arr));
     }
 }
