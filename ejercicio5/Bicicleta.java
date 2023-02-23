6d1fbf149a6f9b142ff7f72f4f431c5dba2381f4
Juan Morales <juanguillermo93@gmail.com>
2023-02-23 10:17:23 -0500

    creacion de la clase bicicleta

---
+++ ejercicio5/Bicicleta.java
@@ -0,0 +1,28 @@
+package ejercicio5;
+
+public class Bicicleta extends Vehiculo{
+    private int numeroMarchas;
+
+    public Bicicleta(int numPasajeros, boolean tieneTripulacion, int numRuedas, String fechaMatriculacion,
+            String medioDesplazamiento, String marca, String modelo, String tipoCombustible, int numeroMarchas) {
+        super(numPasajeros, tieneTripulacion, numRuedas, fechaMatriculacion, medioDesplazamiento, marca, modelo,
+                tipoCombustible);
+        this.numeroMarchas = numeroMarchas;
+    }
+
+    public int getNumeroMarchas() {
+        return numeroMarchas;
+    }
+
+    public void setNumeroMarchas(int numeroMarchas) {
+        this.numeroMarchas = numeroMarchas;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche [Numero de Pasajeros=" + getNumPasajeros() + ", Tiene tripulacion=" + isTieneTripulacion() + ", Numero de Ruedas=" + getNumRuedas()
+                + ", fecha de Matriculacion=" + getFechaMatriculacion() + ", medio de Desplazamiento=" + getMedioDesplazamiento()
+                + ", numero de marchas=" + getNumeroMarchas() + "]";
+    }
+
+}
