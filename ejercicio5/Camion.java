ba3756a7128bc8d471b09f531e59e73e6d000066
Juan Morales <juanguillermo93@gmail.com>
2023-02-23 10:14:13 -0500

    creacion de la clase camion

---
+++ ejercicio5/Camion.java
@@ -0,0 +1,27 @@
+package ejercicio5;
+
+public class Camion extends Vehiculo{
+    private int capacidadCarga;
+
+    public Camion(int numPasajeros, boolean tieneTripulacion, int numRuedas, String fechaMatriculacion,
+            String medioDesplazamiento, String marca, String modelo, String tipoCombustible, int capacidadCarga) {
+        super(numPasajeros, tieneTripulacion, numRuedas, fechaMatriculacion, medioDesplazamiento, marca, modelo,
+                tipoCombustible);
+        this.capacidadCarga = capacidadCarga;
+    }
+    
+    public int getCapacidadCarga() {
+        return capacidadCarga;
+    }
+
+    public void setCapacidadCarga(int capacidadCarga) {
+        this.capacidadCarga = capacidadCarga;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche [Numero de Pasajeros=" + getNumPasajeros() + ", Tiene tripulacion=" + isTieneTripulacion() + ", Numero de Ruedas=" + getNumRuedas()
+                + ", fecha de Matriculacion=" + getFechaMatriculacion() + ", medio de Desplazamiento=" + getMedioDesplazamiento()
+                + ", capacidad de carga=" + getCapacidadCarga() + "]";
+    }
+}
