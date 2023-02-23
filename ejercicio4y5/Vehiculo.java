5e00703e5eaca35e7102939e8748a4b33e98e2ac
Juan Morales <juanguillermo93@gmail.com>
2023-02-23 07:29:00 -0500

    creacion de la clase vehiculo

---
+++ ejercicio4y5/Vehiculo.java
@@ -0,0 +1,13 @@
+package ejercicio4y5;
+
+public class Vehiculo {
+    private int numPasajeros;
+    private boolean tieneTripulacion;
+    private int numRuedas;
+    private String fechaMatriculacion;
+    private String medioDesplazamiento;
+    private String marca;
+    private String modelo;
+    private String tipoCombustible;
+
+}
